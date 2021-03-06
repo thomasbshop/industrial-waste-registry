ba15cad97423a8484bffe2ee95f1e040d97417cb
Thomas Juma <thomas.juma@outlook.com>
2021-03-06 19:32:41 +0300

    Add "Company details" model.

--- API/firm/models.py
+++ API/firm/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext as _
 
 # Create your models here.
+class Company(models.Model):
+    """
+    General company details.
+    """
+    STATUS_CHOICES = []
+
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(blank=True, default='', max_length=50)
+    address = models.CharField(_("Address"), max_length=50)
+    street = models.CharField(_("Street/Road"), max_length=50)
+    county = models.CharField(choices=STATUS_CHOICES, max_length=50)
+    sub_county = models.CharField(choices=STATUS_CHOICES, max_length=50)
+    ward = models.CharField(choices=STATUS_CHOICES, max_length=50)
+    location = models.CharField(_("geolocation"), max_length=50)
+
+    class Meta:
+        verbose_name = _("Company")
+        verbose_name_plural = _("Companys")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("company_detail", kwargs={"pk": self.pk})
