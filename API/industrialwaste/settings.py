abccd7dc3daddc89291f9769ce0e14760ade4259
Thomas Juma <thomas.juma@outlook.com>
2021-03-06 18:30:52 +0300

    Add postgresql database.

--- API/industrialwaste/settings.py
+++ API/industrialwaste/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'industrialwaste.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
