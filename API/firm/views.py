e42ad7cc0b7ef75cc0789978ab027478b5572e8c
Thomas Juma <thomas.juma@outlook.com>
2021-03-07 10:20:22 +0300

    Add API views.

--- API/firm/views.py
+++ API/firm/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import generics
 
+from .models import Company, Activities,Waste
+from .serialisers import CompanySerializer, ActivitiesSerializer, WasteSerializer
 # Create your views here.
+class CompanyList(generics.ListCreateAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+
+class CompanyDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+class ActivitiesList(generics.ListCreateAPIView):
+    queryset = Activities.objects.all()
+    serializer_class = ActivitiesSerializer
+
+
+class ActivitiesDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Activities.objects.all()
+    serializer_class = ActivitiesSerializer
+
+class WasteList(generics.ListCreateAPIView):
+    queryset = Waste.objects.all()
+    serializer_class = WasteSerializer
+
+
+class WasteDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Waste.objects.all()
+    serializer_class = WasteSerializer
--- API/industrialwaste/urls.py
+++ API/industrialwaste/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from firm import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/company/', views.CompanyList.as_view(), name='entities-list'),
 ]
