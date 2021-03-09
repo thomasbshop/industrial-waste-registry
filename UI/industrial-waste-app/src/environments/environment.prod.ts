3e5214630c130b20cd042adb7debd72b0cf0cc03
Thomas Juma <thomas.juma@outlook.com>
2021-03-09 05:25:13 +0300

    Update environment variables.

--- UI/industrial-waste-app/src/environments/environment.prod.ts
+++ UI/industrial-waste-app/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://localhost/api',
+  auth: {
+    clientID: 'f5CeUwT1KlB1oblnoskbqLGkyOa200rX',
+    domain: 'industrial-waste-registry.eu.auth0.com', // e.g., you.auth0.com
+    audience: 'https://industrial-waste-registry.eu.auth0.com/api/v2/',
+    auth0RedirectUri: 'http://localhost:4200/callback', // URL to return to after auth0 login
+    auth0ReturnTo: 'http://localhost:4200', // URL to return to after auth0 logout
+    scope: 'openid profile'
+  }
 };
