be4cd13d243110ee81a3b33539e384d826779c05
Thomas Juma <thomas.juma@outlook.com>
2021-03-09 05:51:23 +0300

    Add stepper form container.

---
+++ UI/industrial-waste-app/src/app/pages/form-container/form-container.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+
+import { STEP_ITEMS } from '../../constants/multi-step-form';
+
+@Component({
+  selector: 'app-form-container',
+  templateUrl: './form-container.component.html',
+  styleUrls: ['./form-container.component.scss']
+})
+export class FormContainerComponent implements OnInit {
+
+  constructor() { }
+
+  formContent: any;
+  formData: any;
+  activeStepIndex?: number;
+
+  ngOnInit(): void {
+    this.formContent = STEP_ITEMS;
+    this.formData = {};
+  }
+
+  onFormSubmit(formData: any): void {
+    this.formData = formData;
+
+    // post form data here
+    alert(JSON.stringify(this.formData));
+  }
+
+}
